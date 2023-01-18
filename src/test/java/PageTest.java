18ccb6f6d81c1d70f21147d08d56886424fedf3c
SSwati-21 <121857423+SSwati-21@users.noreply.github.com>
2023-01-18 19:22:58 +0100

    Create PageTest.java

---
+++ src/test/java/PageTest.java
@@ -0,0 +1,38 @@
+package url;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class PageTest {
+	WebDriver driver;
+	
+    @BeforeTest
+    public void beforetest(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Swati\\JavaProjects\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    //    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    //    driver.manage().deleteAllCookies();
+       
+    }
+    
+    @Test
+    public void login() throws InterruptedException {
+	driver.get("http://localhost:9090/java-hello-world/");	        
+	Thread.sleep(10);
+        Assert.assertEquals(driver.getTitle(),"localhost");
+
+    }
+    
+    @AfterTest
+    public void aftertest(){
+            driver.quit();
+    }
+}
